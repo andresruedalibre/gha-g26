8c469112916ae57376a71b7429f14a8e6d86adb3
Andres Ruedalibre <andres.perez.mt@correo.usa.edu.co>
2021-10-28 18:56:32 -0500

    primer commit del g26

---
+++ src/test/java/com/mycompany/gha/g26/Clase1Test.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.gha.g26;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author andresruedalibre
+ */
+public class Clase1Test {
+    
+    public Clase1Test() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    /**
+     * Test of suma method, of class Clase1.
+     */
+    @org.junit.jupiter.api.Test
+    public void testSuma() {
+        System.out.println("suma");
+        Clase1 instance = new Clase1();
+        int result = instance.suma(4, 5);
+        assertEquals(9, result);
+    }
+    
+}
